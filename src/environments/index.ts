919bbefcc4dba4721243229c4c34034b88333cd3
Jasurbek Abdurakhmonov <j.abdurakhmonov@apex.local>
2021-10-28 17:14:27 +0500

    typeorm, swagger setup

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,7 +1,15 @@
 import * as dotenv from 'dotenv';
-
 dotenv.config();
+
 const NODE_ENV: string = process.env.NODE_ENV || 'dev';
 const PORT: number = +process.env.PORT || 3000;
+//CRM DB
+const DB_TYPE: string = process.env.DB_TYPE || 'postgres';
+const DB_HOST: string = process.env.DB_HOST || 'localhost';
+const DB_PORT: number = +process.env.DB_PORT || 5432;
+const DB_USER: string = process.env.DB_USER || 'username';
+const DB_PASS: string = process.env.DB_PASS || 'password';
+const DB_NAME: string = process.env.DB_NAME || 'crm_db';
+const DB_ENTITY_PATH: string = process.env.DB_ENTITY_PATH || '/../**/**/entities/**.entity{.ts,.js}';
 
-export { NODE_ENV, PORT };
+export { NODE_ENV, PORT, DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, DB_ENTITY_PATH };
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { PORT } from '@environments';
 import { logger } from '@configs';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    logger.error('error from Main');
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle("Crm Project Management")
+        .setDescription("Crm Project Management API Docs")
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('docs', app, document);
     await app.listen(PORT);
   } catch (e) {
     Logger.error(e);
